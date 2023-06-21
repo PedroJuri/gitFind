dbd4eefb004ab6c85f0c03084266390e7755951d
PedroJuri <p3dr1n018@outlook.com>
2023-06-20 21:43:04 -0300

    Importando o background, input e busca

--- src/pages/Home/Index.js
+++ src/pages/Home/Index.js
@@ -1,9 +1,20 @@
 import { Header } from "../../components/Header";
+import './styles.css';
+import background from '../../assets/background.png';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <div className="conteudo">
+        <img src={background} className="background" alt="background-app"/>
+        <div className="info">
+          <div>
+            <input name="usuario" placeholder="@username"/>
+            <button>Buscar</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
