cbad6ea35464b08a6a1b61b72318eb0832f9b989
PedroJuri <p3dr1n018@outlook.com>
2023-06-20 22:14:19 -0300

    estrutura pronta

--- src/pages/Home/Index.js
+++ src/pages/Home/Index.js
@@ -1,6 +1,7 @@
 import { Header } from "../../components/Header";
-import './styles.css';
 import background from '../../assets/background.png';
+import ItemList from "../../components/ItemList";
+import './styles.css';
 
 function App() {
   return (
@@ -22,6 +23,12 @@ function App() {
             </div>
           </div>
           <hr/>
+          <div>
+            <h4 className="repositorio">Repositórios</h4>
+            <ItemList title="Teste1" description="Teste de descrição"/>
+            <ItemList title="Teste1" description="Teste de descrição"/>
+            <ItemList title="Teste1" description="Teste de descrição"/>
+          </div>
         </div>
       </div>
     </div>
