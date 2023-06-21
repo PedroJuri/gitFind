cb41ca4311789542368e70b3e436263899dc24bd
PedroJuri <p3dr1n018@outlook.com>
2023-06-20 21:57:42 -0300

    feita a parte do perfil do usuário

--- src/pages/Home/Index.js
+++ src/pages/Home/Index.js
@@ -7,12 +7,21 @@ function App() {
     <div className="App">
       <Header/>
       <div className="conteudo">
-        <img src={background} className="background" alt="background-app"/>
+        <img src={background} className="background" alt="background app"/>
         <div className="info">
           <div>
             <input name="usuario" placeholder="@username"/>
             <button>Buscar</button>
           </div>
+          <div className="perfil">
+            <img src="https://avatars.githubusercontent.com/u/73957555?v=4" className="profile" alt="imagem do perfil"/>
+            <div>
+              <h3>Pedro Henrique</h3>
+              <span>@PedroJuri</span>
+              <p>Descrição</p>
+            </div>
+          </div>
+          <hr/>
         </div>
       </div>
     </div>
